f280a2c092a9db2e34750be08086e8168b0bf22a
Mradul Singhal <singhalmradul@gmail.com>
2023-10-24 16:06:40 -0400

    first commit

---
+++ src/main/java/io/github/singhalmradul/highfive/HighFiveApplication.java
@@ -0,0 +1,13 @@
+package io.github.singhalmradul.highfive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighFiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighFiveApplication.class, args);
+	}
+
+}
